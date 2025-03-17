0b2b033c417ee8eb11d470517ed7765e06f77133
Pmacdon15 <pmacdonald15@gmail.com>
2025-03-17 12:56:45 -0600

    added components

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -4,8 +4,8 @@ import { Todo } from '@/types/types';
 export async function GET() {
     try {
         const sql = neon(`${process.env.DATABASE_URL}`);
-        const result = await sql('SELECT * FROM NTODOs');
-        return new Response(JSON.stringify(result as Todo[]), { headers: { 'Content-Type': 'application/json' } });
+        const result = await sql('SELECT * FROM NTODOs');        
+        return new Response(JSON.stringify(result as Todo[]), { headers: { 'Content-Type': 'application/json' } });        
     } catch (error) {
         return new Response(`Error getting comments: ${error}`, { headers: { 'Content-Type': 'text/plain' } });
     }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,13 @@ import { DatePicker } from '@/components/ui/data-picker/date-picker';
 import { Button } from "@/components/ui/button"
 import { addTodo } from '@/actions/actions';
 import { useState } from 'react';
+import { Checkbox } from "@/components/ui/checkbox"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
 
-export default function Home() {
+export default function Page() {
 
-  const { data, isPending } = useGetTodos();
+  const { data, isPending, isError: isErrorLoading } = useGetTodos();
   const notCompleteTodos = data?.filter((todo) => { if (!todo.complete) return true; });
   const completeTodos = data?.filter((todo) => { if (todo.complete) return true })
   const [dueDate, setDueDate] = useState<Date | undefined>(undefined);
@@ -20,10 +23,10 @@ export default function Home() {
 
   return (
     <div className="flex flex-col min-h-full w-full justify-center items-center">
-      <h1 className="text-4xl mt-4 font-bold ">Todo&apos;s</h1>
+      <h1 className="text-4xl mt-4 font-bold ">Pat&apos;s Todo&apos;s</h1>
       <NewTodoForm setDueDate={setDueDate} addDateToAction={addDateToAction} />
-      <Todos todos={notCompleteTodos} isPending={isPending} toggleTodo={mutateToggleComplete} />
-      <Todos todos={completeTodos} isPending={isPending} toggleTodo={mutateToggleComplete} />
+      <Todos sectionName="Current" todos={notCompleteTodos} isPending={isPending} toggleTodo={mutateToggleComplete} isErrorLoading={isErrorLoading} />
+      <Todos sectionName="Complete" todos={completeTodos} isPending={isPending} toggleTodo={mutateToggleComplete} isErrorLoading={isErrorLoading} />
     </div>
   );
 }
@@ -35,8 +38,8 @@ function NewTodoForm({ setDueDate, addDateToAction }: { setDueDate: (date: Date 
       <form
         action={addDateToAction}        >
         <div className='flex p-4 gap-4'>
-          <input name="todo-name" className='w-2/6 h-10 border shadow-md rounded-md p-2 ' placeholder="Name of todo" />
-          <textarea name="todo-description" className='w-3/6 border shadow-md rounded-md p-2 ' placeholder='Description' />
+          <Input name="todo-name" className='w-2/6 h-10 p-2 ' placeholder="Name of todo" />
+          <Textarea name="todo-description" className='w-3/6 p-2 ' placeholder='Description' />
           <DatePicker callback={setDueDate} />
         </div>
         <div className="flex justify-end">
@@ -47,19 +50,27 @@ function NewTodoForm({ setDueDate, addDateToAction }: { setDueDate: (date: Date 
   )
 }
 
-function Todos({ todos, isPending, toggleTodo }: { todos: Todo[] | undefined, isPending: boolean, toggleTodo: (id: number) => void }) {
+interface TodosProps {
+  sectionName: string;
+  todos: Todo[] | undefined;
+  isPending: boolean;
+  isErrorLoading?: boolean;
+  toggleTodo: (id: number) => void;
+}
+
+function Todos({ sectionName, todos, isPending, isErrorLoading, toggleTodo }: TodosProps) {
   if (isPending) return <div className="border rounded-md w-3/6 mt-4 p-8"><h1>Loading.....</h1></div>
+  if (isErrorLoading) return <div className="border rounded-md w-3/6 mt-4 p-8"><h1>Error Loading</h1></div>
   return (
     <div className="border rounded-md w-3/6 mt-4 p-8 shadow-md">
-      <h1 className="text-2xl mx-auto">Current Todos</h1>
+      <h1 className="text-2xl mx-auto">{sectionName} Todos</h1>
       <ul className="ml-4">
         {todos?.map((todo) => (
           <li
-            className='flex gap-2'
+            className='flex gap-2 items-end'
             key={todo.id}>
-            <input
+            <Checkbox
               defaultChecked={todo.complete}
-              type="checkbox"
               onClick={() => toggleTodo(todo.id)}
             />
             <p className={todo.complete ? 'line-through' : ''}>
