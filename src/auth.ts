11ca29ca14f9b24600c32e95c76dbf328e7699e1
Pmacdon15 <pmacdonald15@gmail.com>
2025-03-18 00:00:46 -0600

    added gitlab oauth

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import GitLab from 'next-auth/providers/gitlab'
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-    providers: [GitHub],
+    providers: [GitHub, GitLab],
 })
