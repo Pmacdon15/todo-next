fed40881297ecc7abf8e7e82ec643e07bb8d73b1
Pmacdon15 <pmacdonald15@gmail.com>
2025-03-17 18:32:14 -0600

    code clean up

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -14,22 +14,6 @@ export async function toggleComplete(id: number) {
     }
 }
 
-
-// export async function addTodo(dueDate: Date, formData: FormData) {
-//     const todoName = formData.get('todo-name')?.toString() || '';
-//     const todoDescription = formData.get('todo-description')?.toString() || '';
-
-//     try {
-//         const sql = neon(`${process.env.DATABASE_URL}`);
-//         await sql(`
-//             INSERT INTO NTODOS (todoName, todoDescription, dueDate, complete)
-//             VALUES ($1, $2, $3, $4);
-//           `, [todoName, todoDescription, dueDate, false]);
-//     } catch (e) {
-//         console.error("Error adding todo: ", e);
-//     }
-// }
-
 export async function addTodo(dueDate: Date, formData: FormData) {
     const todoName = formData.get('todo-name')?.toString() || '';
     const todoDescription = formData.get('todo-description')?.toString() || '';
--- src/app/todo/[user-email]/page.tsx
+++ src/app/todo/[user-email]/page.tsx
@@ -3,7 +3,7 @@ import TodoClientComponent from '@/components/ui/todo-client/todo-client-compone
 
 export default function Page() {
     return (
-        <div className="flex flex-col min-h-full w-full justify-center items-center">
+        <div className="flex flex-col min-h-full w-full justify-center items-center ">
             <h1 className="text-4xl mt-4 font-bold ">Pat&apos;s Todo&apos;s</h1>
             <TodoClientComponent />
         </div>
