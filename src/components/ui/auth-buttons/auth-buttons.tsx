9d81d3fff5d71e0169097cbab3ade7a60d9197c1
Pmacdon15 <pmacdonald15@gmail.com>
2025-03-18 00:02:52 -0600

    added git lab logo

--- src/components/ui/auth-buttons/auth-buttons.tsx
+++ src/components/ui/auth-buttons/auth-buttons.tsx
@@ -2,7 +2,7 @@
 import { loginGithub, loginGitlab, logout } from '@/actions/auth'
 import { Button } from '@/components/ui/button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGithub , faGitlab} from '@fortawesome/free-brands-svg-icons';
 import { Session } from '@auth/core/types';
 
 interface Props {
@@ -20,7 +20,7 @@ export default function AuthButtons({ session }: Props) {
                             Sign In With GitHub
                         </Button>
                         <Button onClick={() => loginGitlab()}>
-                            <FontAwesomeIcon icon={faGithub} className="mr-2" />
+                            <FontAwesomeIcon icon={faGitlab} className="mr-2" />
                             Sign In With GitLab
                         </Button>
                     </>
